4468451830d05d715abdf940bb645e937222f394
TitanGeek <86037044+TitanGeek@users.noreply.github.com>
2024-10-18 01:39:42 +0530

    Day 2

---
+++ day2.java
@@ -0,0 +1,8 @@
+class Test {
+    public static void main(String[] args) {
+        int x = 7; // Initializing Variables
+        String name = "Shreyas";
+        System.out.println(name);
+        System.out.println(x);
+    }
+}
