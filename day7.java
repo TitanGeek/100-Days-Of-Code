e88fc42e3f7074ea5101658aaaadd1c243ee356c
TitanGeek <86037044+TitanGeek@users.noreply.github.com>
2024-10-23 09:38:24 +0530

    Day 7

---
+++ day7.java
@@ -0,0 +1,14 @@
+class Day7 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.sqrt(y));
+        float z = (float) x;
+        System.out.println(Math.floor(z));
+        System.out.println(Math.ceil(z));
+        double a = (double) y;
+        System.out.println(Math.abs(a));
+    }
+}
