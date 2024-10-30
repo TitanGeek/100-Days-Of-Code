a493ee7f8377dbc8d66df55123f3536f0ed8cdc8
TitanGeek <86037044+TitanGeek@users.noreply.github.com>
2024-10-30 10:27:58 +0530

    Day 12

---
+++ day12.java
@@ -0,0 +1,10 @@
+class Day12 {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("Hello");
+        str.append(" World!");
+        System.out.println(str);
+        StringBuilder st = new StringBuilder("Hello");
+        st.append(" World!");
+        System.out.println(st);
+    }
+}
