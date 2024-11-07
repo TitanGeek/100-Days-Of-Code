66b6697f1871a2f06a956d8b9fb50dfdf3f84dbc
TitanGeek <86037044+TitanGeek@users.noreply.github.com>
2024-11-07 18:32:24 +0530

    Day 19

---
+++ day19.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+class Day19 {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        int c = ran.nextInt(100);
+        System.out.println(c);
+    }
+}
